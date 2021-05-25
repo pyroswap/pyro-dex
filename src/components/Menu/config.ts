b66af5fc5afad885469ecbd8fa8d3b26d0b8b219
pyroswap <pyroswap@protonmail.com>
2021-05-25 12:02:56 +0200

    first commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,102 @@
+import { MenuEntry } from '@pyroswap/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://pancakeswap.finance/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    status: {
+      text: 'MIGRATE',
+      color: 'warning',
+    },
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.pancakeswap.finance/#/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.pancakeswap.finance/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://pancakeswap.finance/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://pancakeswap.finance/pools',
+  },
+  {
+    label: 'Lottery',
+    icon: 'TicketIcon',
+    href: 'https://pancakeswap.finance/lottery',
+  },
+    {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'Overview',
+        href: 'https://pancakeswap.info',
+      },
+      {
+        label: 'Tokens',
+        href: 'https://pancakeswap.info/tokens',
+      },
+      {
+        label: 'Pairs',
+        href: 'https://pancakeswap.info/pairs',
+      },
+      {
+        label: 'Accounts',
+        href: 'https://pancakeswap.info/accounts',
+      },
+    ],
+  },
+  {
+    label: 'IFO',
+    icon: 'IfoIcon',
+    href: 'https://pancakeswap.finance/ifo',
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Contact',
+        href: 'https://docs.pancakeswap.finance/contact-us',
+      },
+      {
+        label: 'Voting',
+        href: 'https://voting.pancakeswap.finance',
+      },
+      {
+        label: 'Github',
+        href: 'https://github.com/pancakeswap',
+      },
+      {
+        label: 'Docs',
+        href: 'https://docs.pancakeswap.finance',
+      },
+      {
+        label: 'Blog',
+        href: 'https://pancakeswap.medium.com',
+      },
+      {
+        label: 'Merch',
+        href: 'https://pancakeswap.creator-spring.com/',
+      },
+    ],
+  },
+]
+
+export default config
---
+++ src/constants/localisation/languageCodes.ts
@@ -0,0 +1,53 @@
+export const AR = { locale: 'ar-SA', language: 'العربية', code: 'ar' }
+export const EN = { locale: 'en-US', language: 'English', code: 'en' }
+export const DE = { locale: 'de-DE', language: 'Deutsch', code: 'de' }
+export const EL = { locale: 'el-GR', language: 'Ελληνικά', code: 'el' }
+export const ESES = { locale: 'es-ES', language: 'Español', code: 'es-ES' }
+export const FI = { locale: 'fi-FI', language: 'Suomalainen', code: 'fi' }
+export const FIL = { locale: 'fil-PH', language: 'Filipino', code: 'fil' }
+export const FR = { locale: 'fr-FR', language: 'Français', code: 'fr' }
+export const HI = { locale: 'hi-IN', language: 'हिंदी', code: 'hi' }
+export const HU = { locale: 'hu-HU', language: 'Magyar', code: 'hu' }
+export const ID = { locale: 'id-ID', language: 'Bahasa Indonesia', code: 'id' }
+export const IT = { locale: 'it-IT', language: 'Italiano', code: 'it' }
+export const JA = { locale: 'ja-JP', language: '日本語', code: 'ja' }
+export const KO = { locale: 'ko-KR', language: '한국어', code: 'ko' }
+export const NL = { locale: 'nl-NL', language: 'Nederlands', code: 'nl' }
+export const PTBR = { locale: 'pt-BR', language: 'Português (Brazil)', code: 'pt-br' }
+export const RO = { locale: 'ro-RO', language: 'Română', code: 'ro' }
+export const RU = { locale: 'ru-RU', language: 'Русский', code: 'ru' }
+export const SVSE = { locale: 'sv-SE', language: 'Svenska', code: 'sv' }
+export const TA = { locale: 'ta-IN', language: 'தமிழ்', code: 'ta' }
+export const TR = { locale: 'tr-TR', language: 'Türkçe', code: 'tr' }
+export const UK = { locale: 'uk-UA', language: 'Українська', code: 'uk' }
+export const VI = { locale: 'vi-VN', language: 'Tiếng Việt', code: 'vi' }
+export const ZHCN = { locale: 'zh-CN', language: '简体中文', code: 'zh-cn' }
+export const ZHTW = { locale: 'zh-TW', language: '繁體中文', code: 'zh-tw' }
+
+export const allLanguages = [
+  AR,
+  EN,
+  DE,
+  EL,
+  ESES,
+  FI,
+  FIL,
+  FR,
+  HI,
+  HU,
+  ID,
+  IT,
+  JA,
+  KO,
+  NL,
+  PTBR,
+  RO,
+  RU,
+  SVSE,
+  TA,
+  TR,
+  UK,
+  VI,
+  ZHCN,
+  ZHTW,
+]
